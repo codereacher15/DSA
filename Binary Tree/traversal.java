f36d2981a917ee8334de4527dcea596a67bd7a95
codereacher15 <77222463+codereacher15@users.noreply.github.com>
2023-02-05 00:42:22 +0530

    Update traversal.java
    Inorder,Postorder,Preorder and level order traversal implementation

--- Binary Tree/traversal.java
+++ Binary Tree/traversal.java
@@ -22,12 +22,37 @@ public class traversal{
 		postorder(node.left);
 		postorder(node.right);
     System.out.print(node.data+" ");
-    
+   public static  ArrayList<ArrayList<Integer>> bfst(implement.BTree node) {
+		 
+		  Queue<implement.BTree> qu=new LinkedList<>();
+		  ArrayList<ArrayList<Integer>> ar=new ArrayList<>();
+		  if(node == null) {
+				 return ar;
+			 }
+		 
+		  qu.offer(node);
+		  while(!qu.isEmpty()) {
+			  int s=qu.size();
+			  ArrayList<Integer> level=new ArrayList<>();
+			  for(int i=0;i<s;i++) {
+				  implement.BTree n=qu.poll();  
+				  level.add(n.data);
+				  if(n.left!=null)
+				  qu.offer(n.left);
+				  if(n.right!=null)
+				  qu.offer(n.right);
+			  }
+			  ar.add(level);
+		  }
+		  
+		  return ar; 
+	 }  
    public static void main(String[] args) {
 	   
      implement.root=new implement.BTree(10,new implement.BTree(20,
 	             new implement.BTree(40),new implement.BTree(50)),
 	          new implement.BTree(30,
 	    		 new implement.BTree(70),new implement.BTree(80)));
-	  postorder2(implement.root);
+	  postorder(implement.root);
+	  System.out.print(bfst(implement.root)); 
 	}
