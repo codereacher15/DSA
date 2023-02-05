6e9ea632aaa2dc873d1915e5d3594fba666d67b2
codereacher15 <77222463+codereacher15@users.noreply.github.com>
2023-02-05 10:32:59 +0530

    Update iterative_trav.java

--- Binary Tree/iterative_trav.java
+++ Binary Tree/iterative_trav.java
@@ -66,6 +66,41 @@ public class iterative_trav {
 		}
 		
 	}
+	static void all_trav(implement.BTree node) {
+		Stack<Pair> stk=new Stack<>();
+		List<Integer> pre=new ArrayList<>();
+		List<Integer> post=new ArrayList<>();
+		List<Integer> in=new ArrayList<>();
+	    if(node==null) { 
+	    	return;
+	    }
+		stk.push(new Pair(node,1));
+	    while(!stk.isEmpty()) {
+	    	Pair it=stk.pop();
+	    	if(it.second==1) {
+	    		pre.add(it.first.data);
+	    		it.second++;
+	    		stk.push(it);
+	    		if(it.first.left!=null) {
+	    			stk.push(new Pair(it.first.left,1));
+	    		}
+	    	}
+	    	else if(it.second==2) {
+	    		in.add(it.first.data);
+	    		it.second++;
+	    		stk.push(it);
+	    		if(it.first.right!=null) {
+	    			stk.push(new Pair(it.first.right,1));
+	    		}
+	    	}
+	    	else if(it.second==3) {
+	    		post.add(it.first.data);
+	    	}
+	    }
+	    System.out.println("Preorder :"+pre);
+	    System.out.println("PostOrder :"+post);
+	    System.out.println("Inorder :"+in);
+	}
    public static void main(String[] args) {
 	   implement.root=new implement.BTree(10,new implement.BTree(20,
 	             new implement.BTree(40),new implement.BTree(50)),
